eca77f47935987c1259f20304503c97e2be8e4b5
= <=>
2017-08-19 15:59:40 +0100

    Moved Transform Functionality to Entity. Parent/Child relationship to be implemented here in future.

--- gpupro-6/Entity.cpp
+++ gpupro-6/Entity.cpp
@@ -4,14 +4,15 @@
 
 Entity::Entity()
 {
-	m_transform = new Transform();
+	Matrix4x4::MatrixIdentity(&m_translation);
+	Matrix4x4::MatrixIdentity(&m_scale);
+	Matrix4x4::MatrixIdentity(&m_rotation);
 	m_components = new std::vector<Component*>();
 }
 
 
 Entity::~Entity()
 {
-	SAFE_DELETE(m_transform);
 	for each (Component* c in *m_components)
 	{
 		SAFE_DELETE(c);
@@ -19,12 +20,35 @@ Entity::~Entity()
 	SAFE_DELETE(m_components);
 }
 
-const Transform* Entity::GetTransform()
-{
-	return m_transform;
-}
 
 void Entity::AddComponent(Component* c)
 {
 	m_components->push_back(c);
+}
+
+
+const std::vector<Component*>* Entity::GetAllComponents()
+{
+	return m_components;
+}
+
+void Entity::SetTranslation(Vector3 position)
+{
+	m_translation.M41 = position.X;
+	m_translation.M42 = position.Y;
+	m_translation.M43 = position.Z;
+}
+
+
+void Entity::SetScale(Vector3 scale)
+{
+	m_scale.M11 = scale.X;
+	m_scale.M22 = scale.Y;
+	m_scale.M33 = scale.Z;
+}
+
+
+const Matrix4x4 Entity::GetTransformationMatrix()
+{
+	return m_translation * m_scale * m_rotation;
 }
--- gpupro-6/Entity.h
+++ gpupro-6/Entity.h
@@ -1,7 +1,8 @@
 #pragma once
 
 #include <vector>
-#include "Transform.h"
+#include "Matrix.h"
+#include "Vector3.h"
 #include "Component.h"
 
 class Entity
@@ -10,11 +11,21 @@ public:
 	Entity();
 	~Entity();
 
-	const Transform* GetTransform();
 	void AddComponent(Component* c);
+	const std::vector<Component*>* GetAllComponents();
+
+	void SetTranslation(Vector3 position);
+	//void SetRotation(Quaternion rot); //TODO: Quaternions
+	void SetScale(Vector3 scale);
+	// TODO: Add Child / Parent
+
+	const Matrix4x4 GetTransformationMatrix();
 
 private:
-	const Transform* m_transform;
+	Matrix4x4 m_scale;
+	Matrix4x4 m_rotation;
+	Matrix4x4 m_translation;
+
 	std::vector<Component*>* m_components;
 };
 
