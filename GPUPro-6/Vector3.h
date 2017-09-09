94d12e00b8fb6d9cebbbc6d05e670235c0c2b869
Joseph Le Grice <joe.legrice@gmail.com>
2017-09-09 12:53:51 +0100

    Added common vector operations to vector2 + vector3 structs

--- GPUPro-6/Vector3.h
+++ GPUPro-6/Vector3.h
@@ -1,15 +1,71 @@
 #pragma once
 #include "D3D11.h"
+#include <math.h>
 
 struct Vector3
 {
 	FLOAT X;
 	FLOAT Y;
 	FLOAT Z;
-};
 
-struct Vector2
-{
-	FLOAT X;
-	FLOAT Y;
+	Vector3 operator+(const Vector3& other)
+	{
+		Vector3 result;
+		result.X = X + other.X; result.Y = Y + other.Y; result.Z = Z + other.Z;
+		return result;
+	}
+
+	Vector3 operator-(const Vector3& other)
+	{
+		Vector3 result;
+		result.X = X - other.X; result.Y = Y - other.Y; result.Z = Z - other.Z;
+		return result;
+	}
+
+	Vector3 operator*(const float other)
+	{
+		Vector3 result;
+		result.X = X * other; result.Y = Y * other; result.Z = Z * other;
+		return result;
+	}
+
+
+	Vector3 operator/(const float other)
+	{
+		Vector3 result;
+		result.X = X / other; result.Y = Y / other; result.Z = Z / other;
+		return result;
+	}
+
+	void operator+=(const Vector3 other) { X += other.X; Y += other.Y; Z += other.Z; }
+	void operator-=(const Vector3 other) { X -= other.X; Y -= other.Y; Z -= other.Z; }
+	void operator*=(const float other) { X *= other; Y *= other; Z *= other; }
+	void operator/=(const float other) { X /= other; Y /= other; Z /= other; }
+
+	FLOAT Magnitude()
+	{
+		return sqrt(X * X + Y * Y + Z * Z);
+	}
+
+	void Normalize()
+	{
+		FLOAT mag = Magnitude();
+		X /= mag;
+		Y /= mag;
+		Z /= mag;
+	}
+
+	static FLOAT Dot(Vector3 a, Vector3 b)
+	{
+		return a.X * b.X + a.Y * b.Y * a.Z * b.Z;
+	}
+
+	static Vector3 Cross(Vector3 a, Vector3 b)
+	{
+		Vector3 result;
+		result.X = a.Y * b.Z - a.Z * b.Y;
+		result.Y = a.Z * b.X - a.X * b.Z;
+		result.Z = a.X * b.Y - a.Y * b.X;
+		return result;
+	}
 };
---
+++ gpupro-6/Vector2.h
@@ -0,0 +1,60 @@
+#pragma once
+#include "D3D11.h"
+#include <math.h>
+
+struct Vector2
+{
+	FLOAT X;
+	FLOAT Y;
+
+	Vector2 operator+(const Vector2& other)
+	{
+		Vector2 result;
+		result.X = X + other.X; result.Y = Y + other.Y;
+		return result;
+	}
+
+	Vector2 operator-(const Vector2& other)
+	{
+		Vector2 result;
+		result.X = X - other.X; result.Y = Y - other.Y;
+		return result;
+	}
+
+	Vector2 operator*(const float other)
+	{
+		Vector2 result;
+		result.X = X * other; result.Y = Y * other;
+		return result;
+	}
+
+
+	Vector2 operator/(const float other)
+	{
+		Vector2 result;
+		result.X = X / other; result.Y = Y / other;
+		return result;
+	}
+
+	void operator+=(const Vector2 other) { X += other.X; Y += other.Y; }
+	void operator-=(const Vector2 other) { X -= other.X; Y -= other.Y; }
+	void operator*=(const float other) { X *= other; Y *= other; }
+	void operator/=(const float other) { X /= other; Y /= other; }
+
+	FLOAT Magnitude()
+	{
+		return sqrt(X * X + Y * Y);
+	}
+
+	void Normalize()
+	{
+		FLOAT mag = Magnitude();
+		X /= mag;
+		Y /= mag;
+	}
+
+	static FLOAT Dot(Vector2 a, Vector2 b)
+	{
+		return a.X * b.X + a.Y * b.Y;
+	}
+};
