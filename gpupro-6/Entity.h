ace8f286f91f43fa3f0a7071616197b435960460
Joseph Le Grice <joe.legrice@gmail.com>
2017-09-28 23:56:02 +0300

    Moving Entity toward POD component

--- gpupro-6/Entity.h
+++ gpupro-6/Entity.h
@@ -8,31 +8,29 @@
 typedef std::vector<size_t> IndexList;
 typedef std::unordered_map<std::type_index, IndexList> IndexMap;
 
-class Entity
+struct Entity
 {
 public:
-	//template<class T> T& AddComponent();
-	//template<class T> void RemoveComponent();
-	IndexList GetIndicesForComponent(std::type_index);
-	
+	template<class T>
+	void Entity::RegisterComponent(Entity& e, size_t index)
+	{
+		m_componentIndexMap[typeid(T)].push_back(index);
+	}
+
+	template<typename T>
+	static IndexList Entity::GetIndicesForComponent(Entity& e)
+	{
+		std::type_index ti = typeid(T);
+		if (e.m_componentIndexMap.count(ti))
+		{
+			return m_componentIndexMap[ti];
+		}
+		else
+		{
+			return std::vector<size_t>();
+		}
+	}
+
 private:
 	IndexMap m_componentIndexMap;
-
-	Entity();
-	~Entity();
-	Entity(const Entity&) = delete;
 };
-
-//template<class T>
-//T& Entity::AddComponent()
-//{
-//	size_t index = SceneManagementSystem::Instance()->GetSceneGraph()->AddComponent<T>();
-//	m_componentIndexMap[typeid(T)].push_back(index);
-//	return *SceneManagementSystem::Instance()->GetSceneGraph()->GetComponent<T>(index);
-//}
-//
-//template<class T>
-//void Entity::RemoveComponent()
-//{
-//	SceneManagementSystem::Instance()->GetSceneGraph()->Remo<T>(index);
-//}
