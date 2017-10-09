5e3696926732eba547294b112ff003b9a068fe74
Joseph Le Grice <joe.legrice@gmail.com>
2017-10-09 23:28:35 +0100

    Fixed MATERIAL_BUFFER struct

--- gpupro-6/Graphics/ConstantBuffers/ConstantBufferDefines.h
+++ gpupro-6/Graphics/ConstantBuffers/ConstantBufferDefines.h
@@ -2,6 +2,7 @@
 
 #include "ConstantBuffer.h"
 #include "Matrix4x4.h"
+#include "Vector2.h"
 #include "Vector4.h"
 
 #define NUM_BUFFERS 2
@@ -17,24 +18,28 @@ struct MATERIAL_BUFFER
 	Vector4 GlobalAmbient;
 	Vector4 AmbientColor;
 	Vector4 EmissiveColor;
-	FLOAT DiffuseColor;
+	Vector4 DiffuseColor;
 	Vector4 SpecularColor;
 	Vector4 Reflectance;
+	
 	FLOAT Opacity;
 	FLOAT SpecularPower;
 	FLOAT IndexOfRefraction;
 	BOOL HasAmbientTexture;
+
 	BOOL HasEmissiveTexture;
 	BOOL HasDiffuseTexture;
 	BOOL HasSpecularTexture;
 	BOOL HasSpecularPowerTexture;
+	
 	BOOL HasNormalTexture;
 	BOOL HasBumpTexture;
 	BOOL HasOpacityTexture;
 	FLOAT BumpIntensity;
+	
 	FLOAT SpecularScale;
 	FLOAT AlphaThreshold;
-	FLOAT Padding;
+	Vector2 Padding;
 };
 
 typedef ConstantBuffer<PER_OBJECT_BUFFER, 0, NUM_BUFFERS, BIND_ALL> PerObjectBuffer;
