04f6d83434254787a66e95d3c33b680691133ef8
Joseph Le Grice <joe.legrice@gmail.com>
2017-10-09 18:36:20 +0100

    Added some safety to ConstantBuffer

--- GPUPro-6/ConstantBuffer.h
+++ GPUPro-6/ConstantBuffer.h
@@ -7,17 +7,20 @@
 enum BindFlags
 {
 	BIND_VERTEX = 1 << 0,
-	BIND_HULL = 1 << 1,
+	BIND_HULL   = 1 << 1,
 	BIND_DOMAIN = 1 << 2,
-	BIND_GEOM = 1 << 3,
-	BIND_PIXEL = 1 << 4,
-	BIND_ALL = BIND_VERTEX | BIND_HULL | BIND_DOMAIN | BIND_GEOM | BIND_PIXEL
+	BIND_GEOM   = 1 << 3,
+	BIND_PIXEL  = 1 << 4,
+	BIND_ALL    = BIND_VERTEX | BIND_HULL | BIND_DOMAIN | BIND_GEOM | BIND_PIXEL
 };
 
 template<class T, size_t m_bufferSlot, size_t m_maxBuffers, BindFlags m_bindFlags>
 class ConstantBuffer
 {
 	friend class ConstantBufferManagementSystem;
+	
+	static_assert(sizeof(T) % 16 == 0, "Constant Buffer size must be divisible by 16");
+	static_assert(sizeof(T) <= D3D11_REQ_CONSTANT_BUFFER_ELEMENT_COUNT, "Constant Buffer size must be less than or equal to D3D11_REQ_CONSTANT_BUFFER_ELEMENT_COUNT");
 
 public:
 	void UpdateBuffer(T& data)
