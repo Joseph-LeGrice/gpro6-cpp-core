f17c5a2268255ac10967cfbe6260a0aae1baf080
Joseph Le Grice <joe.legrice@gmail.com>
2018-01-14 22:36:19 +0000

    Added PostBuildUtil.py - need to implement it!

---
+++ PostBuildUtil.py
@@ -0,0 +1,13 @@
+# import shutil
+
+BUILD_DIR = "./Game/build/x64-Debug/"
+RESOURCE_DIRECTORIES = [
+    "./GPUPro-6/src/Engine/Shaders",
+    "./Game/srd/Resources"
+]
+
+print('TODO: Implement PostBuildUtil!')
+
+# @staticmethod
+# def CopyResources():
+#     for dir in RESOURCE_DIRECTORIES:
