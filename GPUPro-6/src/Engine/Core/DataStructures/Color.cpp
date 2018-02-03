e69e991888dcd83396e7f8308923c2d83849bb2f
Joseph Le Grice <joe.legrice@gmail.com>
2018-02-03 17:16:24 +0000

    Added a couple of constructors to Color

--- GPUPro-6/src/Engine/Core/DataStructures/Color.cpp
+++ GPUPro-6/src/Engine/Core/DataStructures/Color.cpp
@@ -1,3 +1,13 @@
 #include "stdafx.h"
 #include "Color.h"
 
+Color operator*(Color rhs, float lhs)
+{
+    Color c = Color(
+        rhs.R * lhs,
+        rhs.G * lhs,
+        rhs.B * lhs,
+        rhs.A * lhs
+    );
+    return rhs;
+}
--- GPUPro-6/src/Engine/Core/DataStructures/Color.h
+++ GPUPro-6/src/Engine/Core/DataStructures/Color.h
@@ -7,9 +7,15 @@ struct Color
     FLOAT B;
     FLOAT A;
 
-    static Color Red()   { return{ 1.0f, 0.0f, 0.0f, 1.0f }; }
-    static Color Green() { return{ 0.0f, 1.0f, 0.0f, 1.0f }; }
-    static Color Blue()  { return{ 0.0f, 0.0f, 1.0f, 1.0f }; }
-    static Color White() { return{ 1.0f, 1.0f, 1.0f, 1.0f }; }
-    static Color Black() { return{ 0.0f, 0.0f, 0.0f, 1.0f }; }
+    static Color Red()   { return { 1.0f, 0.0f, 0.0f, 1.0f }; }
+    static Color Green() { return { 0.0f, 1.0f, 0.0f, 1.0f }; }
+    static Color Blue()  { return { 0.0f, 0.0f, 1.0f, 1.0f }; }
+    static Color White() { return { 1.0f, 1.0f, 1.0f, 1.0f }; }
+    static Color Black() { return { 0.0f, 0.0f, 0.0f, 1.0f }; }
+
+    Color() : R(0), G(0), B(0), A(0) { }
+    Color(float r, float g, float b) : R(r), G(g), B(b), A(1) { }
+    Color(float r, float g, float b, float a) : R(r), G(g), B(b), A(a) { }
 };
+
+Color operator*(Color rhs, float lhs);
