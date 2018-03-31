fa60c3db3f7aae5afd58100f59dec9127cff77a2
Joseph Le Grice <joe.legrice@gmail.com>
2018-03-31 16:29:27 +0100

    Combined config files

---
+++ BuildUtils/src/data/config-files.ts
@@ -0,0 +1,25 @@
+
+export interface Config
+{
+    dlls?: DllConfig[]
+    resources?: ResourceConfig[];
+    monoProjects?: MonoBuildConfig[];
+}
+
+export interface DllConfig 
+{
+    [arch: string]: ResourceConfig;
+};
+
+export interface ResourceConfig
+{
+    relativeSourceDirectory: string;
+    relativeTargetDirectory: string;
+};
+
+export interface MonoBuildConfig
+{
+    relativeProjectDirectory: string;
+    relativeTargetDirectory: string;
+    libraries: string[];
+};
