8c2b5cbceebc5d47ede8d41b4888f76394ed458d
Joseph Le Grice <joe.legrice@gmail.com>
2018-04-11 23:05:19 +0100

    Oops that method doesn't exist anymore!

--- MonoScripts/Program.cs
+++ MonoScripts/Program.cs
@@ -3,16 +3,9 @@ using System.Runtime.CompilerServices;
 
 class Program
 {
-    [MethodImplAttribute(MethodImplOptions.InternalCall)]
-    private extern static void Test();
-
    static int Main(string[] args)
     {
-        Test();
-        Console.WriteLine("Hello World");
         TestSystem test = new TestSystem();
-        Console.WriteLine("Made de object");
-
         return 0;
     }
 }
