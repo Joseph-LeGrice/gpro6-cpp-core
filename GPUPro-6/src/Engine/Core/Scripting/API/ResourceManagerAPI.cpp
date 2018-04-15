f5db23be1e479a20ca5cf5c0c7ca19b834a59514
Joseph Le Grice <joe.legrice@gmail.com>
2018-04-15 15:46:30 +0100

    Added ResourceManagerAPI

---
+++ GPUPro-6/src/Engine/Core/Scripting/API/ResourceManagerAPI.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include "ResourceManagerAPI.h"
+#include "Engine/Core/ResourceManagement/ResourceTypeMapping.h"
+#include "Engine/Core/ResourceManagement/ResourceManager.h"
+#include "Engine/Core/GlobalStaticReferences.h"
+
+MonoObject* ResourceManagerAPI::CreateResource(MonoString* className)
+{
+	const char* classNameReal = mono_string_to_utf8(className);
+	ResourceTypeID resourceId = ResourceTypeMappings::Instance().GetResourceType(classNameReal);
+	IResource* newResourceUnmanaged = GlobalStaticReferences::Instance()->GetResourceManager()->CreateResource(resourceId);
+	return newResourceUnmanaged->GetManagedObject();
+}
+
+void ResourceManagerAPI::RegisterMonoMethods()
+{
+	mono_add_internal_call("ResourceManagerAPI::CreateResource", ResourceManagerAPI::CreateResource);
+	//mono_add_internal_call("ResourceManagerAPI::DestroyResource", ResourceManagerAPI::DestroyResource);
+	//mono_add_internal_call("ResourceManagerAPI::GetAllResourcesOfType", ResourceManagerAPI::GetAllResourcesOfType);
+	//mono_add_internal_call("ResourceManagerAPI::GetResource", ResourceManagerAPI::GetResource);
+}
---
+++ GPUPro-6/src/Engine/Core/Scripting/API/ResourceManagerAPI.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#pragma warning(push)
+#pragma warning(disable:4201)
+#include <mono/metadata/object.h>
+#pragma warning(pop)
+
+class ResourceManagerAPI //TODO
+{
+public:
+	static MonoObject* CreateResource(MonoString* className);
+	//static void DestroyResource(MonoType* object);
+	//static MonoArray* GetAllResourcesOfType(MonoType resourceType);
+	//static MonoObject* GetResource(int resourceIndex);
+
+	static void RegisterMonoMethods();
+};
