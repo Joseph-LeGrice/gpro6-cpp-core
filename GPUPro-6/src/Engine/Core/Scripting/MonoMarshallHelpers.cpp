e19d484d90f849107f172524dbe943f040e72ef1
Joseph Le Grice <joe.legrice@gmail.com>
2018-05-13 10:40:03 +0100

    Implementing MonoMarshall::GetStringVector

--- GPUPro-6/src/Engine/Core/Scripting/MonoMarshallHelpers.cpp
+++ GPUPro-6/src/Engine/Core/Scripting/MonoMarshallHelpers.cpp
@@ -17,6 +17,13 @@ std::wstring MonoMarshall::GetUTF16String(MonoString* ms)
 
 extern std::vector<std::wstring> MonoMarshall::GetStringVector(MonoArray* ma)
 {
-	UNREFERENCED_PARAMETER(ma);
-	return std::vector<std::wstring>();
+	std::vector<std::wstring> result;
+	uintptr_t l = mono_array_length(ma);
+	for (int i = 0; i < l; i++)
+	{
+		MonoString* element = mono_array_get(ma, MonoString*, i);
+		std::wstring strElement = GetUTF16String(element);
+		result.push_back(strElement);
+	}
+	return result;
 }
