e1363ffa61f6681858f66f2b8c5d87d68a3d4de5
Joseph Le Grice <joe.legrice@gmail.com>
2018-05-20 18:04:05 +0100

    added some operator overloads to vector types

--- Scripting/GPro/types/Quaternion.cs
+++ Scripting/GPro/types/Quaternion.cs
@@ -4,4 +4,12 @@ public struct Quaternion
     float x;
     float y;
     float z;
+
+    public Quaternion(float w, float x, float y, float z)
+    {
+        this.w = w;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 }
--- Scripting/GPro/types/Vector2.cs
+++ Scripting/GPro/types/Vector2.cs
@@ -2,4 +2,18 @@ public struct Vector2
 {
     float x;
     float y;
+
+    public Vector2(float x, float y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static Vector2 operator *(Vector2 lhs, float rhs)
+    {
+        Vector2 result;
+        result.x = lhs.x * rhs;
+        result.y = lhs.y * rhs;
+        return result;
+    }
 }
